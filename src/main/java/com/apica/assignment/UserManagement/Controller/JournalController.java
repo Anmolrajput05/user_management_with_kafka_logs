814642583da90f2ac86e892aa8fe3623a2f6e854
Anmol Rajput <33937784+Anmolrajput05@users.noreply.github.com>
2024-04-28 22:19:34 +0530

    Update JournalController.java

--- src/main/java/com/apica/assignment/UserManagement/Controller/JournalController.java
+++ src/main/java/com/apica/assignment/UserManagement/Controller/JournalController.java
@@ -3,6 +3,7 @@ package com.apica.assignment.UserManagement.Controller;
 import com.apica.assignment.UserManagement.Model.Journal;
 import com.apica.assignment.UserManagement.Services.JournalService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
@@ -15,12 +16,14 @@ public class JournalController {
 
     // Endpoint to retrieve all journals
     @GetMapping("/journal")
+    @PreAuthorize("hasRole('ADMIN')")
     public List<Journal> getAllJournals() {
         return journalService.getAllJournals();
     }
 
     // Endpoint to retrieve a specific journal by ID
     @GetMapping("/{journalId}")
+    @PreAuthorize("hasRole('ADMIN')")
     public Journal getJournalById(@PathVariable Long journalId) {
         return journalService.getJournalById(journalId);
     }
